b54b3706face625798afd73696796c22d3759fcc
Flynn Lambrechts <flynn.lambrechts@gmail.com>
2024-11-05 11:07:14 +1100

    tute09 template done

---
+++ tute09_template/chmod_if_public_write.c
@@ -0,0 +1,10 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <sys/stat.h>
+// // #include <sys/types.h>
+// #include <unistd.h>
+
+// stat(char* pathname, struct stat* s)
+// man 7 inode
+
+int main(int argc, char *argv[]) {}
---
+++ tute09_template/print_diary.c
@@ -0,0 +1,8 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+
+// getenv
+// snprintf
+
+int main(void) {}
