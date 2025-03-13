206bddb7c4d051f03e229c47c76ae36754a808ab
Flynn Lambrechts <flynn.lambrechts@gmail.com>
2025-03-14 10:12:54 +1100

    Alternate solution to q3

---
+++ week04/q3_alternate.s
@@ -0,0 +1,60 @@
+
+	.text
+main:
+	li	$a0, 11
+	li	$a1, 13
+	li	$a2, 17
+	li	$a3, 19
+	jal	sum4
+	move	$t0, $v0 # int result = sum4(11, 13, 17, 19);
+
+	li	$v0, 1
+	move	$a0, $t0
+	syscall
+
+	li	$a0, '\n'
+	li	$v0, 11
+	syscall
+main__epilogue:
+	li	$v0, 0
+	jr	$ra
+
+sum4:
+sum4__prologue:
+	begin
+	push	$ra
+	push	$s0
+
+	push	$s2
+	push	$s3
+sum4__body:
+	#	$s0 - res1
+	move	$s2, $a2
+	move	$s3, $a3
+
+	jal	sum2
+	move	$s0, $v0
+
+	move	$a0, $s2
+	move	$a1, $s3
+	jal	sum2
+
+	move	$a1, $v0
+	move	$a0, $s5
+	jal	sum2
+
+sum4__epilogue:
+	pop	$s3
+	pop	$s2
+
+	pop	$s0
+	pop	$ra
+	end
+	jr	$ra
+
+sum2:
+sum2__prologue:
+sum2__body:
+	add	$v0, $a0, $a1
+sum2__epilogue:
+	jr	$ra
