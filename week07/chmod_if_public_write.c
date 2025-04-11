f9b58cb25e4b91e59dad94a5d12a0c85b42887d9
Flynn Lambrechts <flynn.lambrechts@gmail.com>
2025-04-11 10:14:29 +1000

    Finishing off the chmod question

---
+++ week07/chmod_if_public_write.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <sys/stat.h>
+
+int main(int argc, char *argv[]) {
+    for (int i = 1; i < argc; ++i) {
+        struct stat s;
+        stat(argv[i], &s);
+
+        if (s.st_mode & S_IWOTH) {
+            printf("removing public write from %s", argv[i]);
+            chmod(argv[i], s.st_mode & (~S_IWOTH));
+        } else {
+            printf("%s is not publically writable", argv[i]);
+        }
+    }
+    return 0;
+}
