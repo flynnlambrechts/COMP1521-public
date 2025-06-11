12a91dafb2e6e230da602fae9e1d573e84fa06b8
Flynn Lambrechts <flynn.lambrechts@gmail.com>
2025-06-11 12:45:26 +1000

    Prep tute2

---
+++ tute02/q6.c
@@ -0,0 +1,18 @@
+// A simple program demonstrating how to represent a implementing an && in an
+// if-statement in MIPS.
+// This version: C, but a slightly less nice approach.
+
+#include <stdio.h>
+
+int main(void) {
+	int x;
+	printf("Enter a number: ");
+	scanf("%d", &x);
+
+	char* message = "small/big\n";
+	if (x > 100 && x < 1000) {
+		message = "medium";
+	}
+
+	printf("%s", message);
+}
